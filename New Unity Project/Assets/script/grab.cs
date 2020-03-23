b719f3ef6f7bc83664f771f87096b36a3c169649
オルミアナ <yuta0518yuta@gmail.com>
2020-03-23 12:52:03 +0900

    岩を消すスクリプトと隕石の方向変化

--- New Unity Project/Assets/script/grab.cs
+++ New Unity Project/Assets/script/grab.cs
@@ -9,15 +9,15 @@ public class grab : MonoBehaviour {
 
     [SerializeField] private GameObject Playertmp;
 
-    public bool grabflg = false;    //falseだったら停止　//trueだったら動く
+    public bool grabflg = false;    //falseだったら動く　//trueだったら止める
 
     PlayerMo script; //UnityChanScriptが入る変数
 
     Vector3 tmp;
 
-    float x;
-    float y;
-    float z;
+    float gx;   //このゲームオブジェクトの調整用変数
+    float gy;
+    float gz;
 
     //ゲーム起動時に呼び出される
     void Start()
@@ -30,9 +30,9 @@ public class grab : MonoBehaviour {
         tmp = Playertmp.transform.position;
         Playertmp.transform.position = new Vector3(tmp.x, tmp.y, tmp.z);
 
-        x = tmp.x;
-        y = tmp.y;
-        z = tmp.z;
+        gx = 0;
+        gy = 0;
+        gz = 0;
     }
 
     void Update()
@@ -42,30 +42,45 @@ public class grab : MonoBehaviour {
         if(grabflg == true)
         {
             tmp = Playertmp.transform.position;
-            this.transform.position = new Vector3(tmp.x-3, tmp.y+3, tmp.z);
+            //this.transform.position = new Vector3(tmp.x-3, tmp.y+3, tmp.z);
+            this.transform.position = new Vector3(tmp.x + gx, tmp.y + gy, tmp.z + gz);
 
-            if (script.playerU == 1)//上
+            if (Input.GetKey(KeyCode.A) && grabflg == true)
             {
-                //this.transform.position = new Vector3(0, 0.1f, 0);
-                script.playerU = 0;
+
+                //this.transform.position = new Vector3(tmp.x +4  , tmp.y + 3, tmp.z);
+                gx = -3f;
+                gy = 3f;
+                gz = 0;
+
             }
-            if (script.playerU == 2)//左
+
+            if (Input.GetKey(KeyCode.S) && grabflg == true)
             {
-                //this.transform.position += new Vector3(-0.1f, 0, 0);
-                script.playerU = 0;
-             
+
+                //this.transform.position = new Vector3(tmp.x , tmp.y + 4, tmp.z);
+                gx = 0;
+                gy = 4f;
+                gz = 0;
+
             }
-            else if (script.playerU == 3)//右
+
+            if (Input.GetKey(KeyCode.D) && grabflg == true)
             {
-                script.playerU = 0;
-                //this.transform.position += new Vector3(0.1f, 0, 0);
+
+                //this.transform.position = new Vector3(tmp.x +4  , tmp.y + 3, tmp.z);
+                gx = 4f;
+                gy = 3f;
+                gz = 0;
+
             }
+            
             if (Input.GetKey(KeyCode.W) && grabflg == true)
             {
-               
-                //重力を停止させる
+
+                //重力を戻す
                 rb.isKinematic = false;
-                
+
                 grabflg = false;
             }
         }
@@ -76,19 +91,9 @@ public class grab : MonoBehaviour {
 
         if (other.gameObject.tag == "Player")
         {
-            // 上に移動
-            if (Input.GetKey(KeyCode.W) && grabflg == true)
-            {
-               
-
-               //重力を停止させる
-               rb.isKinematic = false;
-                
-                grabflg = false;
-            }
 
             // 上に移動
-            else if (Input.GetKey(KeyCode.Space) && grabflg == false)
+            if (Input.GetKey(KeyCode.Space) && grabflg == false)
             {
                
              //重力を動かす
@@ -96,6 +101,9 @@ public class grab : MonoBehaviour {
                 
                 grabflg = true;
                 this.transform.Translate(0.0f, 1.0f, 0.0f);
+                gx = -3f;
+                gy = 3f;
+                gz = 0;
             }
            
         }
