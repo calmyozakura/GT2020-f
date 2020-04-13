d36900f9a92ce7024c89790a31932fd2da7d4b47
オルミアナ <yuta0518yuta@gmail.com>
2020-04-13 11:47:48 +0900

    build2を作った
    スクリプトをちょっと変更...

--- New Unity Project/Assets/ASSET/yu/sp/CameraCONT.cs
+++ New Unity Project/Assets/ASSET/yu/sp/CameraCONT.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class CameraCONT : MonoBehaviour {
 
+    //[SerializeField] private GameObject Playertmp;
+
     public Transform target;//キャラの座標の入れ物
     public Vector3 offset;//キャラからの距離
                           // Use this for initialization
+
+    //PlayerMo script; //UnityChanScriptが入る変数
+
+    //Vector3 tmp;
+
     void Start()
     {
+        //script = Playertmp.GetComponent<PlayerMo>(); //unitychanの中にあるUnityChanScriptを取得して変数に格納する
 
+        //tmp = Playertmp.transform.position;
+        //Playertmp.transform.position = new Vector3(tmp.x, tmp.y, tmp.z);
     }
 
     // Update is called once per frame
