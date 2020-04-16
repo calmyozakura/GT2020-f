35161974526640701de567af453062e8b0883b38
ki-ri- <kiri0204p@gmail.com>
2020-04-16 14:44:16 +0900

    アロのモデルとモーションの追加

---
+++ New Unity Project/Assets/ASSET/ki/Scripts/BreakTreeCont.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakTreeCont : MonoBehaviour
+{
+    public Animator BTree_anim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+          if(Input.GetKeyDown("c"))
+            {
+                BTree_anim.SetBool("BreakFlg", true);
+            }
+           
+
+        }
+    }
+}
