f805f227ce570f1fe9ed65957299865f099043a6
tatsuki929 <11807113@stu.kbc.ac.jp>
2020-05-07 21:27:33 +0900

    ステージ遷移　作成中

---
+++ New Unity Project/Assets/ASSET/isa/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    Scene scene;
+
+    // Use this for initialization
+    void Start () {
+        scene = SceneManager.GetActiveScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SceneManager.LoadScene(scene.name);
+    }
+}
