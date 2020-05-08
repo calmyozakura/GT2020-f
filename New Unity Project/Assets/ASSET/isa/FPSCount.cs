dffe3bbb9c19462ec28aa23d2550b3c80cf722b8
tatsuki929 <11807113@stu.kbc.ac.jp>
2020-05-08 14:13:15 +0900

    FPS表示の可視化？ VBlank → Don't Syncとし、スクリプトによる60FPS化を図る。

---
+++ New Unity Project/Assets/ASSET/isa/FPSCount.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCount
+{
+    private float Interval = 0.5f; //計測頻度
+    private float Accum    = 0.0f; //蓄積値
+    private float Rest     = 0.0f; //残り時間
+    private float fps      = 0.0f; //計算結果
+
+    private int   Frame    = 0;    //フレーム値
+
+
+    /*----------------------------------------------------*/
+    public FPSCount()
+    {
+
+    }
+
+    public void CountUpDate()
+    {
+        Rest  -= Time.deltaTime;
+        Accum += Time.timeScale / Time.deltaTime;
+        Frame ++;
+
+        if(Rest <= 0.0f)
+        {
+            fps = Accum / Frame;
+            Rest = Interval;
+            Accum = 0.0f;
+            Frame = 0;
+        }
+    }
+
+    public float getfps()
+    {
+        return fps;
+    }
+
+    
+}
---
+++ New Unity Project/Assets/ASSET/isa/FPSView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSView : MonoBehaviour
+{
+    private FPSCount count = null;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        count = new FPSCount();
+        
+    }
+
+    private void Update()
+    {
+        count.CountUpDate();
+    }
+
+    void OnGUI()
+    {
+        string str;
+
+
+        str = "FPS:";
+        str += count.getfps().ToString();
+
+        GUI.Label(new Rect(100, 0, 100, 50), str);
+    }
+}
