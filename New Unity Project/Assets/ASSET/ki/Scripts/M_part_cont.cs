0d57ab050b5a4463a60cc7528dd0b633ee87496d
ki-ri- <kiri0204p@gmail.com>
2020-05-21 11:33:25 +0900

    プレイヤーが死ぬと画面いっぱいにマグマ

---
+++ New Unity Project/Assets/ASSET/ki/Scripts/M_part_cont.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_part_cont : MonoBehaviour
+{
+    public GameObject Dino;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Dino == null)
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+
+   
+}
