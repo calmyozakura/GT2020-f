ffd578f2da348ecce3719236909b672dd2d6ebd6
Abenomics <11806027@stu.kbc.ac.jp>
2020-06-01 18:00:54 +0900

    念願のボス追加

---
+++ New Unity Project/Assets/ASSET/kei/FireIceElemental/SingleDemo.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleDemo: MonoBehaviour {
+	public Transform T;
+	public float speed = 10.0f;
+	
+	public int curmat;
+	
+	bool spin;
+	public bool Fire;
+	
+	Camera cam;
+
+	public Animator anim;
+	public float size = 1;
+	public Material[] Mat;
+	public Renderer mesh;
+	public bool FX;
+	public GameObject[] FX01;
+	public GameObject[] FX02;
+	public GameObject[] DieAnim;
+	public GameObject Elemental;
+	
+	void Start () {
+		cam = Camera.main;
+		spin = false;
+		transform.localScale = new Vector3(size,size,size);
+		anim = GetComponentInChildren<Animator>();
+		DeactivateDieMesh();
+		CheckStartFX();
+	}
+	
+	
+	public void Spin(){
+		spin = !spin;
+		T.eulerAngles = Vector3.zero;
+	}
+	public void ChangeMat(){
+		if(curmat == 0){
+			curmat = 1;
+			if(FX){
+				for ( int i = 0; i < FX01.Length; i++){
+					FX01[i].SetActive(false);
+				}
+				for ( int i = 0; i < FX02.Length; i++){
+					FX02[i].SetActive(true);
+				}
+			}
+		}else{
+			curmat = 0;
+			if(FX){
+				for ( int i = 0; i < FX01.Length; i++){
+					FX01[i].SetActive(true);
+				}
+				for ( int i = 0; i < FX02.Length; i++){
+					FX02[i].SetActive(false);
+				}
+			}
+		}
+		mesh.material = Mat[curmat];
+	}
+		
+		/// animation
+	public void Idle(){
+
+		CheckStatus();
+		anim.Play("idle");
+	}
+
+	public void IdleActivate(){
+		
+		CheckStatus();
+		anim.Play("idleActivate");
+	}
+
+	public void Walk(){
+		CheckStatus();
+		anim.Play("walk");
+	}
+	public void Run(){
+		CheckStatus();
+		anim.Play("run");
+	}
+	public void Hit(){
+		CheckStatus();
+		anim.Play("hit");
+	}
+	public void Activate(){
+		CheckStatus();
+		anim.Play("activate");
+	}
+
+	public void Atk01(){
+		CheckStatus();
+		anim.Play("attack01");
+	}
+	public void Atk02(){
+		CheckStatus();
+		anim.Play("attack02");
+	}
+		
+	public void die(){
+		CheckStatus();
+		anim.Play("die");
+	}
+	
+	void Update(){
+		
+		if(Input.GetMouseButton(0)){
+			T.eulerAngles = new Vector3(T.eulerAngles.x,T.eulerAngles.y + Input.GetAxis("Mouse X") * -2,T.eulerAngles.z);
+		}
+		
+		if (spin == false){
+			return;
+		}
+		T.eulerAngles = new Vector3(T.eulerAngles.x,T.eulerAngles.y + speed * Time.deltaTime,T.eulerAngles.z);
+	}
+
+	void CheckStatus(){
+		
+		if(Elemental.activeSelf == false){
+			Elemental.SetActive(true);
+			DeactivateDieMesh();
+		}
+	}
+
+	void DeactivateDieMesh(){
+
+		for(int i = 0 ; i < DieAnim.Length; i++){
+			DieAnim[i].SetActive(false);
+		}
+	}
+
+	void CheckStartFX(){
+			if(curmat == 0){
+				if(FX){
+					for ( int i = 0; i < FX01.Length; i++){
+						FX01[i].SetActive(true);
+					}
+					for ( int i = 0; i < FX02.Length; i++){
+						FX02[i].SetActive(false);
+					}
+				}
+			}else{
+				if(FX){
+					for ( int i = 0; i < FX01.Length; i++){
+						FX01[i].SetActive(false);
+					}
+					for ( int i = 0; i < FX02.Length; i++){
+						FX02[i].SetActive(true);
+					}
+				}
+			}
+		mesh.material = Mat[curmat];
+	}
+}
---
+++ New Unity Project/Assets/ASSET/kei/FireIceElemental/animEvent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animEvent : MonoBehaviour {
+
+	public GameObject[] DieAnim;
+	public GameObject FireElemental;
+	public GameObject[] Explosion;
+	public Transform Istancer;
+
+	SingleDemo Demo;
+
+	void Start(){
+		Demo = GetComponentInParent<SingleDemo>();
+	}
+
+	public void ExplosionEvent(){
+		if(Demo.curmat == 0){
+			GameObject G = (GameObject)Instantiate(Explosion[0], Istancer.position, Quaternion.identity);
+			G.SetActive(true);
+		}else{
+			GameObject G = (GameObject)Instantiate(Explosion[1], Istancer.position, Quaternion.identity);
+			G.SetActive(true);
+		}
+	}
+
+
+	public void DieEvent(){
+
+		if(Demo.curmat == 0){
+			DieAnim[0].SetActive(true);
+		}else{
+			DieAnim[1].SetActive(true);
+		}
+
+		FireElemental.SetActive(false);
+
+	}
+}
