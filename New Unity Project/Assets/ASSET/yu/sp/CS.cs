23fcb6d6e38b2166661c04470e5f0a53e765e12c
オルミアナ <yuta0518yuta@gmail.com>
2020-06-04 15:58:38 +0900

    一旦あげたる

---
+++ New Unity Project/Assets/ASSET/yu/sp/CS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS : MonoBehaviour {
+
+    public CameraShake shake;
+
+    private void Update()
+    {
+        
+            shake.Shake(0.5f, 0.5f);
+        
+    }
+}
