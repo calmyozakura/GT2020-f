417cb9ebcad54726157764d4da640ba704262f4a
tatsuki929 <11807113@stu.kbc.ac.jp>
2020-06-06 17:08:33 +0900

    とりあえず木への仮対応

--- New Unity Project/Assets/ASSET/isa/SE_Break.cs
+++ New Unity Project/Assets/ASSET/isa/SE_Break.cs
@@ -7,12 +7,14 @@ public class SE_Break : MonoBehaviour {
 
     private float WaitTimer = 0;
     bool SE_BREAK=false;
-
+    int s;
     public AudioSource[] sources;
+
 
 	// Use this for initialization
 	void Start () {
-		//sources = gameObject.GetComponents<AudioSource>();
+        //sources = gameObject.GetComponents<AudioSource>();
+        s = 0;
 	}
 
     private void OnTriggerStay(Collider other)
@@ -30,19 +32,21 @@ public class SE_Break : MonoBehaviour {
 
         
         
-            if (other.gameObject.tag == "Player")
+            if (other.gameObject.tag == "Player"&&s==0)
             {
                 if (Input.GetKey(KeyCode.C)
                  || Input.GetButton("DS4_Circle"))
                 {
                     if (++WaitTimer / 6 == 2)
                     {
-                       sources[0].Play();
-                        WaitTimer = 0;
+                       sources[s].Play();
+                        WaitTimer = 0;s = 1;
                     }
                 }
+            
             }
-    }
 
-    
+
+    }
+   
 }
---
+++ New Unity Project/Assets/ASSET/isa/pwwp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pwwp : MonoBehaviour {
+
+    AudioSource[] sources;
+
+	// Use this for initialization
+	void Start () {
+        sources = this.gameObject.GetComponents<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+                    sources[0].Play();                
+            
+        }
+    }
+}
