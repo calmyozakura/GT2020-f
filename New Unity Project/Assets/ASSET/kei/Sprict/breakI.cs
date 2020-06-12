7b9e5bef61a8117a242c309bf3ebb608295d9b36
ki-ri- <kiri0204p@gmail.com>
2020-06-12 11:08:21 +0900

    ステージ３の修正　丸い岩

--- New Unity Project/Assets/ASSET/kei/Sprict/breakI.cs
+++ New Unity Project/Assets/ASSET/kei/Sprict/breakI.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class breakI : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public GameObject iwa;
+    public GameObject part1;
+    private Vector3 Iwa_posi;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Iwa_posi = this.transform.position;
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -20,6 +22,14 @@ public class breakI : MonoBehaviour {
         {
             Destroy(gameObject);
 
+        }
+        
+        if (other.gameObject.tag == "Yougan")
+        {
+            part1.gameObject.SetActive(false);
+            part1.transform.position = Iwa_posi;
+            part1.gameObject.SetActive(true);//パーティクルをアクティブに
+            Destroy(this.gameObject);//デストロイ
         }
     }
 
